fd32328f4d4d7686418dd89ce7dbdbc818c7b6f0
mikailylk <mikail.yoelek@student.htl-rankweil.at>
2023-01-21 16:05:37 +0100

    Fixed Orientation
    Quaternion endgültiger fix und neue Euler-Reihung

--- ICU-App/ICU-App/Calc/AngleCalc.cs
+++ ICU-App/ICU-App/Calc/AngleCalc.cs
@@ -12,9 +12,9 @@ namespace ICU_App.Calc
         #region c
         public AngleCalc()
         {
-
         }
         #endregion
+
 
         /// <summary>
         /// returns Vector3 X,Y,Z representing different angles in Euler
@@ -23,74 +23,164 @@ namespace ICU_App.Calc
         /// <returns></returns>
         public Vector3 ToEulerAngles(Quaternion q)
         {
+            // Z-Y-X Konvention (Tayt-Brian)
             Vector3 angles = new();
 
-            //// roll / x
-            //double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-            //double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-            //angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
+            // roll / x
+            double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
+            double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+            angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
 
-            //// pitch / y
-            //double sinp = 2 * (q.W * q.Y - q.Z * q.X);
-            ////if (Math.Abs(sinp) >= 1)
-            ////{
-            ////    angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
-            ////}
-            ////else
-            ////{
-            ////    angles.Y = (float)Math.Asin(sinp);
-            ////}
-            //angles.Y = (float)Math.Asin(sinp);
-
-            //// yaw / z
-            //double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-            //double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-            //angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
-
-            double yaw, pitch, roll;
-
-            double test = q.X * q.Y + q.Z * q.W;
-            if (test > 0.499)
+            // pitch / y
+            double sinp = 2 * (q.W * q.Y - q.Z * q.X);
+            if (Math.Abs(sinp) >= 1)
             {
-                yaw = 2 * Math.Atan2(q.X, q.W);
-                pitch = Math.PI / 2;
-                roll = 0;
-            }
-            else if (test < -0.499)
-            {
-                yaw = -2 * Math.Atan2(q.X, q.W);
-                pitch = -Math.PI / 2;
-                roll = 0;
+                angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
             }
             else
             {
-                double sqx = q.X * q.X;
-                double sqy = q.Y * q.Y;
-                double sqz = q.Z * q.Z;
-                yaw = Math.Atan2(2 * q.Y * q.W - 2 * q.X * q.Z, 1 - 2 * sqy - 2 * sqz);
-                pitch = Math.Asin(2 * test);
-                roll = Math.Atan2(2 * q.X * q.W - 2 * q.Y * q.Z, 1 - 2 * sqx - 2 * sqz);
+                angles.Y = (float)Math.Asin(sinp);
             }
+            //angles.Y = (float)Math.Asin(sinp);
 
-            double yawDegrees = yaw * 180 / Math.PI;
-            double pitchDegrees = pitch * 180 / Math.PI;
-            double rollDegrees = roll * 180 / Math.PI;
-
-            yawDegrees = yawDegrees % 360;
-            if (yawDegrees < 0) yawDegrees += 360;
-
-            pitchDegrees = pitchDegrees % 360;
-            if (pitchDegrees < 0) pitchDegrees += 360;
-
-            rollDegrees = rollDegrees % 360;
-            if (rollDegrees < 0) rollDegrees += 360;
-
-
-            angles.X = (float)rollDegrees;
-            angles.Y = (float)pitchDegrees;
-            angles.Z = (float)yawDegrees;
+            // yaw / z
+            double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+            double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+            angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
 
             return angles;
+        }
+
+        public enum RotSeq
+        {
+            ZYX, ZYZ, ZXY, ZXZ, YXZ, YXY, YZX, YZY, XYZ, XYX, XZY, XZX
+        };
+
+        Vector3 twoaxisrot(float r11, float r12, float r21, float r31, float r32)
+        {
+            Vector3 ret = new Vector3
+            {
+                X = (float)(Math.Atan2(r11, r12) * (180 / Math.PI)),
+                Y = (float)(Math.Acos(r21) * (180 / Math.PI)),
+                Z = (float)(Math.Atan2(r31, r32) * (180 / Math.PI))
+            };
+            return ret;
+        }
+
+        Vector3 threeaxisrot(float r11, float r12, float r21, float r31, float r32)
+        {
+            Vector3 ret = new Vector3
+            {
+                X = (float)(Math.Atan2(r31, r32) * (180 / Math.PI)),
+                Y = (float)(Math.Asin(r21) * (180 / Math.PI)),
+                Z = (float)(Math.Atan2(r11, r12) * (180 / Math.PI))
+            };
+            return ret;
+        }
+
+        public Vector3 quaternion2Euler(Quaternion q, RotSeq rotSeq)
+        {
+            switch (rotSeq)
+            {
+                case RotSeq.ZYX:
+                    return threeaxisrot(2 * (q.X * q.Y + q.W * q.Z),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z,
+                        -2 * (q.X * q.Z - q.W * q.Y),
+                        2 * (q.Y * q.Z + q.W * q.X),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z);
+
+
+                case RotSeq.ZYZ:
+                    return twoaxisrot(2 * (q.Y * q.Z - q.W * q.X),
+                        2 * (q.X * q.Z + q.W * q.Y),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z,
+                        2 * (q.Y * q.Z + q.W * q.X),
+                        -2 * (q.X * q.Z - q.W * q.Y));
+
+
+                case RotSeq.ZXY:
+                    return threeaxisrot(-2 * (q.X * q.Y - q.W * q.Z),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.Y * q.Z + q.W * q.X),
+                        -2 * (q.X * q.Z - q.W * q.Y),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z);
+
+
+                case RotSeq.ZXZ:
+                    return twoaxisrot(2 * (q.X * q.Z + q.W * q.Y),
+                        -2 * (q.Y * q.Z - q.W * q.X),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z,
+                        2 * (q.X * q.Z - q.W * q.Y),
+                        2 * (q.Y * q.Z + q.W * q.X));
+
+
+                case RotSeq.YXZ:
+                    return threeaxisrot(2 * (q.X * q.Z + q.W * q.Y),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z,
+                        -2 * (q.Y * q.Z - q.W * q.X),
+                        2 * (q.X * q.Y + q.W * q.Z),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z);
+
+                case RotSeq.YXY:
+                    return twoaxisrot(2 * (q.X * q.Y - q.W * q.Z),
+                        2 * (q.Y * q.Z + q.W * q.X),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.X * q.Y + q.W * q.Z),
+                        -2 * (q.Y * q.Z - q.W * q.X));
+
+
+                case RotSeq.YZX:
+                    return threeaxisrot(-2 * (q.X * q.Z - q.W * q.Y),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.X * q.Y + q.W * q.Z),
+                        -2 * (q.Y * q.Z - q.W * q.X),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z);
+
+
+                case RotSeq.YZY:
+                    return twoaxisrot(2 * (q.Y * q.Z + q.W * q.X),
+                        -2 * (q.X * q.Y - q.W * q.Z),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.Y * q.Z - q.W * q.X),
+                        2 * (q.X * q.Y + q.W * q.Z));
+
+
+                case RotSeq.XYZ:
+                    return threeaxisrot(-2 * (q.Y * q.Z - q.W * q.X),
+                        q.W * q.W - q.X * q.X - q.Y * q.Y + q.Z * q.Z,
+                        2 * (q.X * q.Z + q.W * q.Y),
+                        -2 * (q.X * q.Y - q.W * q.Z),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z);
+
+
+                case RotSeq.XYX:
+                    return twoaxisrot(2 * (q.X * q.Y + q.W * q.Z),
+                        -2 * (q.X * q.Z - q.W * q.Y),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.X * q.Y - q.W * q.Z),
+                        2 * (q.X * q.Z + q.W * q.Y));
+
+
+                case RotSeq.XZY:
+                    return threeaxisrot(2 * (q.Y * q.Z + q.W * q.X),
+                        q.W * q.W - q.X * q.X + q.Y * q.Y - q.Z * q.Z,
+                        -2 * (q.X * q.Y - q.W * q.Z),
+                        2 * (q.X * q.Z + q.W * q.Y),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z);
+
+
+                case RotSeq.XZX:
+                    return twoaxisrot(2 * (q.X * q.Z - q.W * q.Y),
+                        2 * (q.X * q.Y + q.W * q.Z),
+                        q.W * q.W + q.X * q.X - q.Y * q.Y - q.Z * q.Z,
+                        2 * (q.X * q.Z + q.W * q.Y),
+                        -2 * (q.X * q.Y - q.W * q.Z));
+
+                default:
+                    return Vector3.Zero;
+
+            }
+
         }
     }
 }
--- ICU-App/ICU-App/ViewModel/DebugViewModel.cs
+++ ICU-App/ICU-App/ViewModel/DebugViewModel.cs
@@ -1,7 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿// using Android.Views.Animations;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ICU_App.Calc;
 using System.Numerics;
+//using static Android.Resource;
 
 namespace ICU_App.ViewModel;
 
@@ -11,7 +13,20 @@ public partial class DebugViewModel : ObservableObject
 
     SensorSpeed speed;
     AngleCalc calc;
-    Vector3 euler;
+
+    Vector3 angles;
+    Vector3 retAngles;
+
+    Quaternion old_q = new Quaternion(0, 0, 0, 0);
+    Quaternion new_q;
+
+    double deltax = 0;
+    double deltaz = 0;
+
+    double oldx = 0;
+    double oldz = 0;
+
+    double x, y, z;
 
     double standard_x;
     double standard_y;
@@ -35,10 +50,14 @@ public partial class DebugViewModel : ObservableObject
         // Überprüfen ob Orientation Sensor unterstützt wird
         if (this.orientationSensor.IsSupported)
         {
-            speed = SensorSpeed.Fastest;
+            speed = SensorSpeed.UI;
             OrientationSensor.ReadingChanged += OrientationSensor_ReadingChanged;
             calc = new AngleCalc();
-            euler = new Vector3();
+            angles = new Vector3();
+            retAngles = new Vector3();
+            x = 0;
+            y = 0;
+            z = 0;
         }
     }
 
@@ -61,7 +80,7 @@ public partial class DebugViewModel : ObservableObject
                 orientationSensor.Start(speed);
             }
         }
-        
+
     }
 
     private bool CanStart_Stop_Debug()
@@ -76,9 +95,10 @@ public partial class DebugViewModel : ObservableObject
     [RelayCommand(CanExecute = nameof(CanSet_Position))]
     private void Set_Position()
     {
-        standard_x = (euler.X * 180 / Math.PI);
-        standard_y = (euler.Y * 180 / Math.PI);
-        standard_z = (euler.Z * 180 / Math.PI);
+        originQ.X = 0;
+        originQ.Y = 0;
+        originQ.Z = 0;
+        originQ.W = 1;
     }
 
     private bool CanSet_Position()
@@ -90,35 +110,143 @@ public partial class DebugViewModel : ObservableObject
         return false;
     }
 
+    Quaternion originQ = new Quaternion(0,0,0,1);
     private void OrientationSensor_ReadingChanged(object sender, OrientationSensorChangedEventArgs e)
     {
+        // https://stackoverflow.com/questions/54540095/xamarin-orientation-sensor-quaternion
+        
+        if (originQ == Quaternion.Identity)
+        {
+            originQ = Quaternion.Inverse(e.Reading.Orientation);
+        }
+
+        var q = Quaternion.Multiply(originQ, e.Reading.Orientation);
+
+        //angles = calc.ToEulerAngles(q);
+        //angles.Z = (float)(angles.Z * 180 / Math.PI);    // ("horizontale Neigung des Smartphones")
+        //angles.Y = (float)(angles.Y * 180 / Math.PI);    // ("Schrägheit des Smartphones")   
+        //angles.X = (float)(angles.X * 180 / Math.PI);    // ("vertikale Neigung des Smartphones" -> abhängig von Ausrichtung des Smartphones)
+
+        angles = calc.quaternion2Euler(q,AngleCalc.RotSeq.YZX);
+
         var data = e.Reading;
-
-
         // TODO: Binding
         OrientationDebug = "Orient: \n\r" +
             $"X: {Math.Round(data.Orientation.X, 3)}, Y: {Math.Round(data.Orientation.Y, 3)}, Z: {Math.Round(data.Orientation.Z, 3)}, W: {Math.Round(data.Orientation.W, 3)}";
 
-        Quaternion q = new Quaternion((float)Math.Round(data.Orientation.X, 3), (float)Math.Round(data.Orientation.Y, 3), (float)Math.Round(data.Orientation.Z, 3), (float)Math.Round(data.Orientation.W, 3));
-        euler = calc.ToEulerAngles(q);
-        // OrientationEulerDebug = $"Euler\n\rX: {(int)(euler.X * 180 / Math.PI)} \n\rY: {(int)(euler.Y * 180 / Math.PI)} \n\rZ: {(int)(euler.Z * 180 / Math.PI)}";
 
-        //double yaw = euler.Z * 180 / Math.PI;       // ("horizontale Neigung des Smartphones")
-        //double pitch = euler.Y * 180 / Math.PI;     // ("Schrägheit des Smartphones")   
-        //double roll = euler.X * 180 / Math.PI;      // ("vertikale Neigung des Smartphones" -> abhängig von Ausrichtung des Smartphones)
+        ////Quaternion q = new Quaternion((float)data.Orientation.X, 3), (float)Math.Round(data.Orientation.Y, 3), (float)Math.Round(data.Orientation.Z, 3), (float)Math.Round(data.Orientation.W, 3));
 
-        double yaw = euler.Z;
-        double pitch = euler.Y;
-        double roll = euler.X;
+        //Quaternion q = e.Reading.Orientation;
 
+        //Quaternion delta = old_q * Quaternion.Inverse(q);
 
-        OrientationEulerDebug = $"Dir\n\r{nameof(pitch)} {(int)(pitch)} \n\r{nameof(roll)} {(int)(roll)} \n\r{nameof(yaw)} {(int)(yaw)}\n\r";
-        // Richtung (Vertikal, Horizontal Links, Horizontal Rechts)
-        // Y ... -90 Links (Handy aufkippen)
-        // Y ... +90 Rechts (Handy aufkippen)
-        // Z ... Kopf links rechts drehen
+        //old_q = q;
 
-        
+        //angles = calc.ToEulerAngles(delta);
+        //// OrientationEulerDebug = $"Euler\n\rX: {(int)(euler.X * 180 / Math.PI)} \n\rY: {(int)(euler.Y * 180 / Math.PI)} \n\rZ: {(int)(euler.Z * 180 / Math.PI)}";
+
+        //angles.Z = (float)(angles.Z * 180 / Math.PI);   // ("horizontale Neigung des Smartphones")
+        //angles.Y = (float)(angles.Y * 180 / Math.PI);   // ("Schrägheit des Smartphones")   
+        //angles.X = (float)(angles.X * 180 / Math.PI);   // ("vertikale Neigung des Smartphones" -> abhängig von Ausrichtung des Smartphones)
+
+        x = angles.X;
+        y = angles.Y;
+        z = angles.Z;
+        OrientationEulerDebug = $"Dir\n\rPitch: {(double)(y)} \n\rRoll {(double)(x)} \n\rYaw {(double)z}\n\r";
     }
+    //private void OrientationSensor_ReadingChanged(object sender, OrientationSensorChangedEventArgs e)
+    //{
+    //    var data = e.Reading;
+    //    // TODO: Binding
+    //    OrientationDebug = "Orient: \n\r" +
+    //        $"X: {Math.Round(data.Orientation.X, 3)}, Y: {Math.Round(data.Orientation.Y, 3)}, Z: {Math.Round(data.Orientation.Z, 3)}, W: {Math.Round(data.Orientation.W, 3)}";
+
+    //    //Quaternion q = new Quaternion((float)data.Orientation.X, 3), (float)Math.Round(data.Orientation.Y, 3), (float)Math.Round(data.Orientation.Z, 3), (float)Math.Round(data.Orientation.W, 3));
+
+    //    Quaternion q = e.Reading.Orientation;
+
+    //    Quaternion delta = old_q * Quaternion.Inverse(q);
+
+    //    old_q = q;
+
+    //    angles = calc.ToEulerAngles(delta);
+    //    // OrientationEulerDebug = $"Euler\n\rX: {(int)(euler.X * 180 / Math.PI)} \n\rY: {(int)(euler.Y * 180 / Math.PI)} \n\rZ: {(int)(euler.Z * 180 / Math.PI)}";
+
+    //    angles.Z = (float)(angles.Z * 180 / Math.PI);       // ("horizontale Neigung des Smartphones")
+    //    angles.Y = (float)(angles.Y * 180 / Math.PI);     // ("Schrägheit des Smartphones")   
+    //    angles.X = (float)(angles.X * 180 / Math.PI);      // ("vertikale Neigung des Smartphones" -> abhängig von Ausrichtung des Smartphones)
+
+    //    //if (Math.Abs(angles.Y) < 85)
+    //    //{
+    //    //    deltax = angles.X - oldx;
+    //    //    deltaz = angles.Z - oldz;
+
+    //    //    if (deltax < -100)
+    //    //    {
+    //    //        angles.X = angles.X + 180;
+    //    //        retAngles.Y = -90 + (-90 - angles.Y);
+    //    //    }
+    //    //    else
+    //    //    {
+    //    //        retAngles.Y = angles.Y;
+    //    //    }
+    //    //    if (deltaz < -100)
+    //    //    {
+    //    //        angles.Z = angles.Z + 180;
+    //    //    }
+
+    //    //    if (deltax > 100)
+    //    //    {
+    //    //        angles.X = angles.X - 180;
+    //    //        retAngles.Y = 90 + (90 - angles.Y);
+    //    //    }
+
+    //    //    if (deltaz > 100)
+    //    //    {
+    //    //        angles.Z = angles.Z - 180;
+    //    //    }
+
+    //    //    oldx = angles.X;
+    //    //    oldz = angles.Z;
+
+    //    //    angles.X = angles.X + (float)0.1;
+    //    //    angles.Y = angles.Y + (float)0.1;
+    //    //    angles.Z = angles.Z + (float)0.1;
+
+
+    //    //    if (angles.X < 0)
+    //    //    {
+    //    //        retAngles.X = angles.X + 360;
+    //    //    }
+    //    //    else
+    //    //    {
+    //    //        retAngles.X = angles.X;
+    //    //    }
+    //    //    if (angles.Z < 0)
+    //    //    {
+    //    //        retAngles.Z = angles.Z + 360;
+    //    //    }
+    //    //    else
+    //    //    {
+    //    //        retAngles.Z = angles.Z;
+    //    //    }
+
+    //    //    if (retAngles.Y < 0)
+    //    //    {
+    //    //        retAngles.Y = retAngles.Y + 360;
+    //    //    }
+    //    //OrientationEulerDebug = $"Dir\n\r{nameof(pitch)} {(int)(pitch)} \n\r{nameof(roll)} {(int)(roll)} \n\r{nameof(yaw)} {(int)(yaw)}\n\r";
+
+    //    x += angles.X;
+    //    y += angles.Y;
+    //    z += angles.Z;
+    //    OrientationEulerDebug = $"Dir\n\rPitch: {(double)(y)} \n\rRoll {(double)(x)} \n\rYaw {(double)z}\n\r";
+    //}
+
+    // Richtung (Vertikal, Horizontal Links, Horizontal Rechts)
+    // Y ... -90 Links (Handy aufkippen)
+    // Y ... +90 Rechts (Handy aufkippen)
+    // Z ... Kopf links rechts drehen
 }
 
